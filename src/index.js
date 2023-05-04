d845f21cc47eee2eb6ecb5c36e0953311659ab2c
mrcbq <marcobaquero@gmail.com>
2023-05-04 17:52:28 -0500

    Fix bug in webpack configuration that generates an html w linters errors

--- src/index.js
+++ src/index.js
@@ -1,43 +1,49 @@
-import "./index.css";
+import './index.css';
 
 const tasks = [
   {
     id: 0,
-    description: "wash the dishes",
+    description: 'wash the dishes',
     completed: false,
   },
   {
     id: 1,
-    description: "Complete the to-do list project",
+    description: 'Complete the to-do list project',
     completed: false,
   },
   {
     id: 2,
-    description: "take a rest",
+    description: 'take a rest',
     completed: true,
   },
 ];
 
-const tasksContainer = document.getElementById("tasks-container");
+const tasksContainer = document.getElementById('tasks-container');
 
 tasks.forEach((task) => {
-  const listItem = document.createElement("li");
-  const checkbox = document.createElement("input");
-  const textarea = document.createElement("input");
-  const dotsButton = document.createElement("input");
+  const listItem = document.createElement('li');
+  const checkbox = document.createElement('input');
+  const textarea = document.createElement('input');
+  const dotsButton = document.createElement('input');
 
-  checkbox.setAttribute("type", "checkbox");
-  checkbox.setAttribute("tabindex", "0");
-  checkbox.setAttribute("alt", "Check!");
+  checkbox.setAttribute('type', 'checkbox');
+  checkbox.setAttribute('tabindex', '0');
+  checkbox.setAttribute('alt', 'Check!');
 
-  textarea.setAttribute("maxlength", "255");
+  textarea.setAttribute('maxlength', '255');
   textarea.value = task.description;
 
-  const attrs = { type: "button", tabindex: "-1", value: "", title: "click and sostain for rearrange", class: "input-btn-dots"};
+  const attrs = {
+    type: 'button',
+    tabindex: '-1',
+    value: '',
+    title: 'click and sostain for rearrange',
+    class: 'input-btn-dots',
+  };
 
-  for (let attr in attrs) {
-    dotsButton.setAttribute(attr, attrs[attr]);
-  }
+  Object.entries(attrs).forEach(([key, value]) => {
+    dotsButton.setAttribute(key, value);
+  });
 
   listItem.appendChild(checkbox);
   listItem.appendChild(textarea);
